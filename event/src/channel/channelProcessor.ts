0ef949d5b872143ab6276f54eec04407a3e3b766
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-03-08 08:46:07 +0100

    chore(ctc-118): refactor channel processor

--- event/src/channel/channelProcessor.ts
+++ event/src/channel/channelProcessor.ts
@@ -1,6 +1,5 @@
 import { isErrorItem, logger } from 'shared';
 import { Message } from '@commercetools/platform-sdk';
-
 import { ChannelService } from '../services/channelService';
 
 export class ChannelProcessor {
@@ -8,7 +7,7 @@ export class ChannelProcessor {
 
   async processChannel(message: Message & { notificationType?: string }): Promise<void> {
     if (message.resource?.typeId != 'channel') {
-      logger.error(`Could not process CT message - resource.typeId '${message.resource?.typeId}' != 'channel'`);
+      logger.warn(`Could not process CT message - resource.typeId '${message.resource?.typeId}' != 'channel'`);
       return;
     }
 
@@ -20,29 +19,11 @@ export class ChannelProcessor {
 
     try {
       if (notificationType === 'ResourceDeleted') {
-        const tenantFacilityId = message.resourceUserProvidedIdentifiers?.key;
-        if (tenantFacilityId) {
-          const facilityId = await this.channelService.setFacilityOffline(tenantFacilityId);
-          if (facilityId) {
-            logger.info(`Set FFT Facility '${facilityId}' to OFFLINE for CT Channel '${tenantFacilityId}'`);
-          }
-        } else {
-          logger.error(
-            `Could not process CT message - ResourceDeleted: message.resourceUserProvidedIdentifiers.key required`
-          );
-        }
+        this.processChannelDeleted(message);
       } else if (notificationType === 'ResourceCreated' || notificationType === 'ResourceUpdated') {
-        const channelId = message.resource.id;
-        if (channelId) {
-          const facility = await this.channelService.upsertFacility(channelId);
-          if (facility) {
-            logger.info(`Updated FFT Facility '${facility.id}' for CT Channel '${facility.tenantFacilityId}'`);
-          }
-        } else {
-          logger.error(`Could not process CT message - ${notificationType}: resource.id required`);
-        }
+        this.processChannelUpdated(message);
       } else {
-        logger.error(`Could not process CT message - cannot handle notificationType '${notificationType}'`);
+        logger.warn(`Could not process CT message - cannot handle notificationType '${notificationType}'`);
       }
     } catch (err) {
       let statusCode: string | number = 500;
@@ -55,6 +36,30 @@ export class ChannelProcessor {
         `Could not process CT '${notificationType}' message for '${message.resource.typeId}' '${message.resource.id}' '${message.resourceUserProvidedIdentifiers?.key}': ${statusCode} - ${errorMessage}`,
         err
       );
+    }
+  }
+
+  private async processChannelDeleted(message: Message): Promise<void> {
+    const tenantFacilityId = message.resourceUserProvidedIdentifiers?.key;
+    if (tenantFacilityId) {
+      const facilityId = await this.channelService.setFacilityOffline(tenantFacilityId);
+      if (facilityId) {
+        logger.info(`Set FFT Facility '${facilityId}' to OFFLINE for CT Channel '${tenantFacilityId}'`);
+      }
+    } else {
+      logger.warn(`Could not process CT message '${message.id}': message.resourceUserProvidedIdentifiers.key required`);
+    }
+  }
+
+  private async processChannelUpdated(message: Message): Promise<void> {
+    const channelId = message.resource.id;
+    if (channelId) {
+      const facility = await this.channelService.upsertFacility(channelId);
+      if (facility) {
+        logger.info(`Updated FFT Facility '${facility.id}' for CT Channel '${facility.tenantFacilityId}'`);
+      }
+    } else {
+      logger.warn(`Could not process CT message '${message.id}': resource.id required`);
     }
   }
 }
