b17c314a2617d794a2a83a2ea4aed85be2e96683
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-10-07 12:38:44 +0200

    chore: update ESLint

---
+++ event/eslint.config.mjs
@@ -0,0 +1,49 @@
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import eslint from '@eslint/js';
+import jestPlugin from 'eslint-plugin-jest';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['**/node_modules', '**/build/', '**/.nvmrc', '**/api.ts'],
+  },
+  prettierConfig,
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts'],
+    ...jestPlugin.configs['flat/recommended'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+
+      parserOptions: {
+        project: 'tsconfig.*?.json',
+        tsconfigRootDir: '/Users/aerpenbeck/src/commercetools-connector/event',
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      'no-undef': 'error',
+      'no-console': 'error',
+      'no-const-assign': 'error',
+    },
+  },
+];
--- event/jest.setup.cjs
+++ event/jest.setup.cjs
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const matchers = require('jest-extended');
 const { resetAllWhenMocks } = require('jest-when');
 
