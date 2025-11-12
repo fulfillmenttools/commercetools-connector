97d7608f5af6378676d3b8b9b4415dbb057e8bf9
Michael Nimz <57434109+mchl-nmz@users.noreply.github.com>
2025-11-12 15:00:12 +0100

    cleanup

--- shared/src/middleware/errorMiddleware.ts
+++ shared/src/middleware/errorMiddleware.ts
@@ -10,7 +10,7 @@ import { CustomError } from '../errors';
  * @returns
  */
 export const errorMiddleware: ErrorRequestHandler = (error: Error, _: Request, res: Response, _next: NextFunction) => {
-  const isDevelopment = true; // process.env.NODE_ENV === 'development';
+  const isDevelopment = process.env.NODE_ENV === 'development';
 
   if (error instanceof CustomError) {
     res.status(error.statusCode).json({
